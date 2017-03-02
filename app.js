d65da27d79ef168e9788cabea558e26f20d77d89
ftalaev <ftalaev@gmail.com>
2017-03-02 18:03:51 +0300

    'Lalalane'

--- app.js
+++ app.js
@@ -5,25 +5,33 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
-
 var app = express();
+
+var debug = require('debug')('skistream:server'),
+    http = require('http'),
+    SerialPort = require('serialport'),
+    WebSocket = require('ws'),
+    port = 3000;
+
+var server = http.createServer(app)
+server.listen(port);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+app.set('port', port);
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+app.post('/port', function(req, res, next) {
+    res.render('index', { title: 'Express' });
+});
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -43,4 +51,45 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+// app.get('/port', function(req, res, next) {
+//     res.render('index', { title: 'Express' });
+//     console.log(req)
+// });
+
+const wss = new WebSocket.Server({ port: 19003 });
+
+wss.broadcast = function broadcast(data) {
+    wss.clients.forEach(function each(client) {
+        if (client.readyState === WebSocket.OPEN) {
+            client.send(data);
+        }
+    });
+};
+
+(function connectToWSerial() {
+    var serial = new SerialPort('COM1', {
+        parser: SerialPort.parsers.readline('#13')
+    });
+
+    serial.on('open', ()=>{
+        console.log("COM post opened")
+    });
+
+    serial.on('data', (data)=>{
+        console.log("COM data received");
+        console.log(data);
+        setImmediate(() => wss.broadcast(JSON.stringify(data)))
+    });
+
+    serial.on('close', ()=>{
+        console.log("COM post closed")
+    });
+
+    serial.on('error', (e)=>{
+        console.log(`COM post err: ${JSON.stringify(e)}`)
+    });
+
+    serial.on('disconnect', ()=>{
+        console.log("COM post disconnected")
+    });
+})();
