ae6058f49dfefd477bf0e3485cb2630947f1a688
talaevlev <ftalaev@gmail.com>
2017-03-03 13:31:51 +0300

    All done

--- public/options.js
+++ public/options.js
@@ -2,7 +2,7 @@
  * Created by sinires on 02.03.17.
  */
 var options = {
-    socket_address: "ws://"+location.host+":19003/"
+    socket_address: "ws://"+location.hostname+":19003/"
 };
 
 var testString = "Вася иванов 12334КОля Петров 42135Вася иванов 12334КОля Петров 42135Вася иванов 12334КОля Петров 42135";
